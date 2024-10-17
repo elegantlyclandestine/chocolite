555614d790f933fddaf734d9cbf0cd013cd68068
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-17 19:39:00 +0800

    Build base for more essential functions

--- include/chocotables.h
+++ include/chocotables.h
@@ -23,10 +23,11 @@ namespace Chocotables {
     private:
         int pos;
         std::string name;
+        std::vector<std::string> headers;
         std::vector<Chocotables::cell> cells;
     public:
-        table(std::string name, std::vector<Chocotables::cell> cells) :
-            name(name), cells(cells) {}
+        table(std::string name, std::vector<std::string>& headers, std::vector<Chocotables::cell>& cells) :
+            name(name), headers(headers), cells(cells) {}
         ~table() {}
         Chocotables::cell read_from_cell(int col, int row) {
             
@@ -38,6 +39,12 @@ namespace Chocotables {
             
         }
         void move_data_between_cells(Chocotables::cell& source, Chocotables::cell& destination, std::optional<bool> overwrite = std::nullopt) {
+            
+        }
+        void add_header(Chocotables::table& table, std::string header) {
+            
+        }
+        void remove_header(Chocotables::table& table, std::string header) {
             
         }
     };
@@ -58,6 +65,12 @@ namespace Chocotables {
             
         }
         void move_table(Chocotables::table& table, int pos) {
+
+        }
+        std::vector<Chocotables::cell> return_sort_vertical(Chocotables::table& table) {
+            
+        }
+        std::vector<Chocotables::cell> return_sort_horizontal(Chocotables::table& table) {
             
         }
     };
