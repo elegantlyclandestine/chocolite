7e8ab36647c0f877af8322552fa084a48121fd4e
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-17 23:55:27 +0800

    Moved Chocolite query structs to separate file

--- include/chocolite.hpp
+++ include/chocolite.hpp
@@ -13,6 +13,7 @@
 
 #include <container2d.hpp>
 #include <container3d.hpp>
+#include <cl_queries.hpp>
 
 namespace fs = std::filesystem;
 
@@ -25,73 +26,6 @@ namespace Chocolite {
         push3D, pick3D, grab3D
     };
     Chocolite::query check_query(std::string query);
-    struct push2D {
-        // PUSH {data} TO {table} IN {container} AT {x,y}
-        std::string data;
-        std::string container;
-        std::string table;
-        int x;
-        int y;
-    };
-    struct push3D {
-        // PUSH {data} TO {table} IN {container} AT {x,y,z}
-        std::string data;
-        std::string container;
-        std::string table;
-        int x;
-        int y;
-        int z;
-    };
-    struct pick2D {
-        // PICK {data} FROM {table} IN {container}
-        std::string data;
-        std::string container;
-        std::string table;
-    };
-    struct pick3D {
-        // PICK {data} FROM {table} IN {container}
-        std::string data;
-        std::string container;
-        std::string table;
-    };
-    struct grab2D {
-        // GRAB DATA ON {x,y} FROM {table} IN {container}
-        std::string container;
-        std::string table;
-        int x;
-        int y;
-    };
-    struct grab3D {
-        // GRAB DATA ON {x,y,z} FROM {table} IN {container}
-        std::string container;
-        std::string table;
-        int x;
-        int y;
-        int z;
-    };
-    struct make2D {
-        // MAKE {table} WITH HEADERS {headers}
-        std::string name;
-        std::vector<std::string> headers;
-    };
-    struct make3D {
-        // MAKE {stock} WITH HEADERS ({2D headers}, {3D headers})
-        std::string name;
-        std::vector<std::string> headers_2d;
-        std::vector<std::string> headers_3d;
-    };
-    struct insert2D {
-        // INSERT {table} INTO {container} AT POSITION {int}
-        Choco2D::table table;
-        Choco2D::container container;
-        int pos;
-    };
-    struct insert3D {
-        // INSERT {stock} INTO {container} AT POSITION {int}
-        Choco3D::stock stock;
-        Choco3D::container container;
-        int pos;
-    };
     
     // 2D functions
     Choco2D::container load_2d_container(std::string container);
---
+++ include/cl_queries.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#ifndef cl_queries_hpp
+#define cl_queries_hpp
+
+#include <chocolite.hpp>
+
+namespace CLQueries {
+    struct push2D {
+        // PUSH {data} TO {table} IN {container} AT {x,y}
+        std::string data;
+        std::string container;
+        std::string table;
+        int x;
+        int y;
+    };
+    struct push3D {
+        // PUSH {data} TO {table} IN {container} AT {x,y,z}
+        std::string data;
+        std::string container;
+        std::string table;
+        int x;
+        int y;
+        int z;
+    };
+    struct pick2D {
+        // PICK {data} FROM {table} IN {container}
+        std::string data;
+        std::string container;
+        std::string table;
+    };
+    struct pick3D {
+        // PICK {data} FROM {table} IN {container}
+        std::string data;
+        std::string container;
+        std::string table;
+    };
+    struct grab2D {
+        // GRAB DATA ON {x,y} FROM {table} IN {container}
+        std::string container;
+        std::string table;
+        int x;
+        int y;
+    };
+    struct grab3D {
+        // GRAB DATA ON {x,y,z} FROM {table} IN {container}
+        std::string container;
+        std::string table;
+        int x;
+        int y;
+        int z;
+    };
+    struct make2D {
+        // MAKE {table} WITH HEADERS {headers}
+        std::string name;
+        std::vector<std::string> headers;
+    };
+    struct make3D {
+        // MAKE {stock} WITH HEADERS ({2D headers}, {3D headers})
+        std::string name;
+        std::vector<std::string> headers_2d;
+        std::vector<std::string> headers_3d;
+    };
+    struct insert2D {
+        // INSERT {table} INTO {container} AT POSITION {int}
+        Choco2D::table table;
+        Choco2D::container container;
+        int pos;
+    };
+    struct insert3D {
+        // INSERT {stock} INTO {container} AT POSITION {int}
+        Choco3D::stock stock;
+        Choco3D::container container;
+        int pos;
+    };
+}
+
+#endif
