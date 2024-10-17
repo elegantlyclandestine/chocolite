9e4b51e980c93d3cdd60e4ef9ae442209fc5f035
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-18 01:01:46 +0800

    Organised 2D and 3D queries. About to restructure other queries

--- include/cl_queries.hpp
+++ include/cl_queries.hpp
@@ -5,7 +5,11 @@
 #include <chocolite.hpp>
 
 namespace CLQuery {
-    // 2D queries
+    /*
+      * 2D queries
+      * Handles 2D (table) database operations.
+      * Accepts objects in the Choco2D namespace.
+    */
     struct push2D {
         // PUSH {data} TO {table} IN {container} AT {x,y}
         std::any data;
@@ -13,12 +17,6 @@ namespace CLQuery {
         Choco2D::table table;
         int x;
         int y;
-    };
-    struct pick2D {
-        // PICK {data} FROM {table} IN {container}
-        std::any data;
-        Choco2D::container container;
-        Choco2D::table table;
     };
     struct grab2D {
         // GRAB DATA ON {x,y} FROM {table} IN {container}
@@ -34,20 +32,10 @@ namespace CLQuery {
         int x1; int y1;
         int x2; int y2;
     };
-    struct build2D {
-        // BUILD 2D CONTAINER {container}
-        std::string name;
-    };
     struct make2D {
         // MAKE {table} WITH HEADERS {headers}
         std::string name;
         std::vector<std::string> headers;
-    };
-    struct insert2D {
-        // INSERT {table} INTO {container} AT POSITION {int}
-        Choco2D::table table;
-        Choco2D::container container;
-        int pos;
     };
     struct erase2D {
         // ERASE DATA FROM {x, y} ON {table} IN {container}
@@ -56,13 +44,12 @@ namespace CLQuery {
         int x;
         int y;
     };
-    struct wipe2D {
-        // WIPE {table} IN {container}
-        Choco2D::container container;
-        Choco2D::table table;
-    };
 
-    // 3D queries
+    /*
+      * 3D queries
+      * Handles 3D (cuboid) database operations.
+      * Takes objects in the Choco3D namespace.
+    */
     struct push3D {
         // PUSH {data} TO {stock} IN {container} AT {x,y,z}
         std::any data;
@@ -71,12 +58,6 @@ namespace CLQuery {
         int x;
         int y;
         int z;
-    };
-    struct pick3D {
-        // PICK {data} FROM {stock} IN {container}
-        std::any data;
-        Choco3D::container container;
-        Choco3D::stock stock;
     };
     struct grab3D {
         // GRAB DATA ON {x,y,z} FROM {table} IN {container}
@@ -93,21 +74,11 @@ namespace CLQuery {
         int x1; int y1; int z1;
         int x2; int y2; int z2;
     };
-    struct build3D {
-        // BUILD 3D CONTAINER {container}
-        std::string name;
-    };
     struct make3D {
         // MAKE {stock} WITH HEADERS ({2D headers}, {3D headers})
         std::string name;
         std::vector<std::string> headers_2d;
         std::vector<std::string> headers_3d;
-    };
-    struct insert3D {
-        // INSERT {stock} INTO {container} AT POSITION {int}
-        Choco3D::stock stock;
-        Choco3D::container container;
-        int pos;
     };
     struct erase3D {
         // ERASE DATA FROM {x, y, z} ON {table} IN {container}
@@ -116,11 +87,6 @@ namespace CLQuery {
         int x;
         int y;
         int z;
-    };
-    struct wipe3D {
-        // WIPE {stock} IN {container}
-        Choco3D::container container;
-        Choco3D::stock stock;
     };
 }
 
