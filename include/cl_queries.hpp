b98c0291335f697f3fe75823efdd135843559573
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-18 20:00:59 +0800

    Simplify usage comments for Chocolite queries

--- include/cl_queries.hpp
+++ include/cl_queries.hpp
@@ -12,15 +12,15 @@ namespace CLQuery {
         std::string structure_name;
     };
     struct finalise : common {
-        // FINALISE {table/stock/tesseract} IN {container}
+        // FINALISE {data structure} IN {container}
         bool make_uneditable = true;
     };
     struct pick : common  {
-        // PICK {data} FROM {table/stock/tesseract} IN {container}
+        // PICK {data} FROM {data structure} IN {container}
         std::any data;
     };
     struct wipe : common {
-        // WIPE {table/stock/tesseract} IN {container}
+        // WIPE {data structure} IN {container}
         bool delete_everything_on_table = true;
     };
     struct build {
@@ -28,36 +28,36 @@ namespace CLQuery {
         std::string container_name;
     };
     struct insert : common {
-        // INSERT {table/stock/tesseract} INTO {container} AT POSITION {int}
+        // INSERT {data structure} INTO {container} AT POSITION {int}
         int position;
     };
     struct obfuscate : common {
-        // OBFUSCATE {table/stock/tesseract} IN {container}
+        // OBFUSCATE {data structure} IN {container}
         std::string obfuscate_type;
     };
     struct add_header : common {
-        // ADD HEADER {header} TO {table/stock/tesseract} AT {x/y/z} IN {container}
+        // ADD HEADER {header} TO {data structure} AT {x/y/z} IN {container}
         std::string header;
     };
     struct lock : common {
-        // LOCK ELEMENT {coordinates} ON {table/stock/tesseract} IN {container}
+        // LOCK ELEMENT {coordinates} ON {data structure} IN {container}
         bool lock_element = true;
     };
     struct push : common {
-        // PUSH {data} TO {table/stock/tesseract} IN {container} AT {coordinates}
+        // PUSH {data} TO {data structure} IN {container} AT {coordinates}
         std::any data;
     };
     struct grab : common {
-        // GRAB DATA ON {coordinates} FROM {table/stock/tesseract} IN {container}
+        // GRAB DATA ON {coordinates} FROM {data structure} IN {container}
         std::vector<int> coordinates;
     };
     struct grab_range : common {
-        // GRAB DATA RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock/tesseract} IN {container}
+        // GRAB DATA RANGE FROM {coordinates_origin} TO {coordinates_end} ON {data structure} IN {container}
         std::vector<int> coordinates_origin;
         std::vector<int> coordinates_end;
     };
     struct make : common {
-        // MAKE {table/stock/tesseract} WITHOUT LIMIT ON CONTAINER {container}
+        // MAKE {data structure} WITHOUT LIMIT ON CONTAINER {container}
         bool infinite = true;
     };
     struct make_fixed : common {
@@ -65,16 +65,16 @@ namespace CLQuery {
         std::vector<int> limit;
     };
     struct erase : common {
-        // ERASE DATA FROM {coordinates} ON {table/stock/tesseract} IN {container}
+        // ERASE DATA FROM {coordinates} ON {data structure} IN {container}
         std::vector<int> coordinates;
     };
     struct erase_range : common {
-        // ERASE DATA IN RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock/tesseract} IN {container}
+        // ERASE DATA IN RANGE FROM {coordinates_origin} TO {coordinates_end} ON {data structure} IN {container}
         std::vector<int> coordinates_origin;
         std::vector<int> coordinates_end;
     };
     struct remove : common {
-        // REMOVE {TABLE/STOCK/TESSERACT/CONTAINER}
+        // REMOVE {data structure/container}
         bool recursive; // directive used if a data structure is defined generally and not by name
     };
 }
