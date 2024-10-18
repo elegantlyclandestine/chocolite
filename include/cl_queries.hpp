d0b5af6b367a2838416903273dc0a929242342ee
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-18 18:50:56 +0800

    Edit comments to include tesseract as data structure

--- include/cl_queries.hpp
+++ include/cl_queries.hpp
@@ -12,15 +12,15 @@ namespace CLQuery {
         std::string structure_name;
     };
     struct finalise : common {
-        // FINALISE {table/stock} IN {container}
+        // FINALISE {table/stock/tesseract} IN {container}
         bool make_uneditable = true;
     };
     struct pick : common  {
-        // PICK {data} FROM {table/stock} IN {container}
+        // PICK {data} FROM {table/stock/tesseract} IN {container}
         std::any data;
     };
     struct wipe : common {
-        // WIPE {table/stock} IN {container}
+        // WIPE {table/stock/tesseract} IN {container}
         bool delete_everything_on_table = true;
     };
     struct build {
@@ -28,50 +28,54 @@ namespace CLQuery {
         std::string container_name;
     };
     struct insert : common {
-        // INSERT {table/stock} INTO {container} AT POSITION {int}
+        // INSERT {table/stock/tesseract} INTO {container} AT POSITION {int}
         int position;
     };
     struct obfuscate : common {
-        // OBFUSCATE {table/stock} IN {container}
+        // OBFUSCATE {table/stock/tesseract} IN {container}
         std::string obfuscate_type;
     };
     struct add_header : common {
-        // ADD HEADER {header} TO {table/stock} AT {x/y/z} IN {container}
+        // ADD HEADER {header} TO {table/stock/tesseract} AT {x/y/z} IN {container}
         std::string header;
     };
     struct lock : common {
-        // LOCK ELEMENT {coordinates} ON {table/stock} IN {container}
+        // LOCK ELEMENT {coordinates} ON {table/stock/tesseract} IN {container}
         bool lock_element = true;
     };
     struct push : common {
-        // PUSH {data} TO {table/stock} IN {container} AT {coordinates}
+        // PUSH {data} TO {table/stock/tesseract} IN {container} AT {coordinates}
         std::any data;
     };
     struct grab : common {
-        // GRAB DATA ON {coordinates} FROM {table/stock} IN {container}
+        // GRAB DATA ON {coordinates} FROM {table/stock/tesseract} IN {container}
         std::vector<int> coordinates;
     };
     struct grab_range : common {
-        // GRAB DATA RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock} IN {container}
+        // GRAB DATA RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock/tesseract} IN {container}
         std::vector<int> coordinates_origin;
         std::vector<int> coordinates_end;
     };
     struct make : common {
-        // MAKE {table/stock} WITHOUT LIMIT ON CONTAINER {container}
+        // MAKE {table/stock/tesseract} WITHOUT LIMIT ON CONTAINER {container}
         bool infinite = true;
     };
     struct make_fixed : common {
-        // MAKE {table/stock} WITH LIMITS UP TO COORDINATES {coordinates} ON CONTAINER {container}
+        // MAKE {table/stock/tesseract} WITH LIMITS UP TO COORDINATES {coordinates} ON CONTAINER {container}
         std::vector<int> limit;
     };
     struct erase : common {
-        // ERASE DATA FROM {coordinates} ON {table/stock} IN {container}
+        // ERASE DATA FROM {coordinates} ON {table/stock/tesseract} IN {container}
         std::vector<int> coordinates;
     };
     struct erase_range : common {
-        // ERASE DATA IN RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock} IN {container}
+        // ERASE DATA IN RANGE FROM {coordinates_origin} TO {coordinates_end} ON {table/stock/tesseract} IN {container}
         std::vector<int> coordinates_origin;
         std::vector<int> coordinates_end;
+    };
+    struct remove : common {
+        // REMOVE {TABLE/STOCK/TESSERACT/CONTAINER}
+        bool recursive; // directive used if a data structure is defined generally and not by name
     };
 }
 
